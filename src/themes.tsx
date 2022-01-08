487b2a207caab9d42166fa20752d8afa9f5f5c9f
Saranya-jena <saranya@chaosnative.com>
2022-01-08 14:11:57 +0530

    base setup

---
+++ src/themes.tsx
@@ -0,0 +1,17 @@
+import { createTheme } from "@material-ui/core/styles";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5B44BA",
+      light: "#858CDD",
+      dark: "#4028A0",
+    },
+    secondary: {
+      main: "#109B67",
+      light: "#109B6799",
+      dark: "#128359",
+    },
+    text: {},
+  },
+});
+export default theme;
